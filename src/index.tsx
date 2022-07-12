1d8b374760acfe413a20247b27bfcb124c969f6d
KROSY <krosy1337@gmail.com>
2022-07-12 21:46:13 +0300

    Add theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,41 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
+import {createTheme, ThemeProvider} from "@mui/material"
 import "styles/global.scss"
+
+const theme = createTheme({
+    palette: {
+        background: {
+            default: "#000",
+            paper: "#0D0D0D",
+        },
+        text: {
+            primary: "#fff",
+        },
+        grey: {
+            "600": "#707070",
+            "A700": "#0D0D0D",
+        },
+    },
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 600,
+            md: 700,
+            lg: 1200,
+            xl: 1640,
+        },
+    }
+})
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 )
 root.render(
     <React.StrictMode>
-        <App/>
+        <ThemeProvider theme={theme}>
+            <App/>
+        </ThemeProvider>
     </React.StrictMode>
 )
