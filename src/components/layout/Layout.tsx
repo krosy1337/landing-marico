3db89011a5aa87d7208c626e0c65cb21ba7a082b
KROSY <krosy1337@gmail.com>
2022-07-12 22:42:00 +0300

    Update layout
    Add header in layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,10 +1,12 @@
 import React, {FC} from "react"
 import {Outlet} from "react-router-dom"
 import {CssBaseline} from "@mui/material"
+import Header from "../Header"
 
 const Layout: FC = () => {
     return (
         <>
+            <Header/>
             <CssBaseline/>
             <Outlet/>
         </>
