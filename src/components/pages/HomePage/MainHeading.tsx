5a23d66c9d708245301c4ff5f17990d3897d3d9e
Alexey Egorov <krosy1337@gmail.com>
2022-07-26 15:28:34 +0300

    Add adaptive in MainHeading section

--- src/components/pages/HomePage/MainHeading.tsx
+++ src/components/pages/HomePage/MainHeading.tsx
@@ -1,13 +1,30 @@
-import React, {FC} from 'react'
+import React, {FC} from "react"
 import {Container, Typography} from "@mui/material"
 import styles from "styles/home.module.scss"
 
 const MainHeading: FC = () => {
     return (
         <Container maxWidth="lg">
-            <Typography variant="h1" fontFamily="inherit" fontWeight="700" align="center">Own your audience.</Typography>
+            <Typography variant="h1" fontFamily="inherit" fontWeight="700"
+                        align="center"
+                        sx={{
+                            fontSize: {
+                                xs: "1.75rem",
+                                sm: "3rem",
+                                md: "4rem",
+                                lg: "6rem",
+                            },
+                        }}>Own your
+                audience.</Typography>
             <Typography variant="h1" fontFamily="inherit" fontWeight="700" align="center"
-                        className={styles.gradientText}>So you don't lose them.</Typography>
+                        className={styles.gradientText} sx={{
+                fontSize: {
+                    xs: "1.75rem",
+                    sm: "3rem",
+                    md: "4rem",
+                    lg: "6rem",
+                },
+            }}>So you don't lose them.</Typography>
         </Container>
     )
 }
