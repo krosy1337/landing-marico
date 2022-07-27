ac66772abd6017dca707993c9945f031ad219006
Alexey Egorov <krosy1337@gmail.com>
2022-07-27 19:55:27 +0300

    Add adaptive in YourHomePage section

--- src/components/pages/HomePage/YourHomePage.tsx
+++ src/components/pages/HomePage/YourHomePage.tsx
@@ -1,69 +1,256 @@
-import React, {FC} from 'react'
-import {Box, Button, List, ListItem, Stack, Typography} from "@mui/material"
+import React, {FC} from "react"
+import {Box, Button, Container, List, ListItem, Stack, Typography} from "@mui/material"
 import GreyButton from "components/UI/GreyButton"
 import image from "assets/laptop_yourhomepage.jpg"
 
 const YourHomePage: FC = () => {
     return (
-        <Box sx={{display: "flex", alignItems: "flex-start", marginBottom: 10,}}>
-            <Box sx={{marginLeft: "auto", marginRight: 20,}}>
-                <Typography variant="h6" fontFamily="inherit" fontWeight={500} sx={{marginBottom: 5}}>Your
+        <Container maxWidth="lg" sx={{
+            display: "flex",
+            alignItems: {xs: "normal", md: "flex-start"},
+            justifyContent: "space-between",
+            marginBottom: {
+                xs: 4,
+                sm: 6,
+                md: 8,
+                lg: 10,
+            },
+            flexDirection: {
+                xs: "column",
+                md: "row",
+            },
+        }}>
+            <Box sx={{
+                width: {
+                    xs: "100%",
+                    md: "50%",
+                },
+                flexGrow: 0,
+                marginRight: {
+                    xs: 0,
+                    lg: 20,
+                },
+            }}>
+                <Typography variant="h6" fontFamily="inherit" fontWeight={500} sx={{
+                    marginBottom: {
+                        xs: 1,
+                        sm: 2,
+                        md: 4,
+                        lg: 5,
+                    },
+                    fontSize: {
+                        xs: "1rem",
+                        sm: "1.10rem",
+                        md: "1.20rem",
+                        lg: "1.25rem",
+                    }
+                }}>Your
                     Homepage</Typography>
-                <Typography variant="h3" fontFamily="inherit" fontWeight={600} sx={{marginBottom: 1,}}>Your
+                <Typography variant="h3" fontFamily="inherit" fontWeight={600} sx={{
+                    marginBottom: {
+                        xs: 0,
+                        sm: 0.2,
+                        md: 0.5,
+                        lg: 1,
+                    }, fontSize: {
+                        xs: "1.5rem",
+                        sm: "2rem",
+                        md: "2.5rem",
+                        lg: "3rem",
+                    }
+                }}>Your
                     Content.</Typography>
-                <Box sx={{marginBottom: 2,}}>
-                    <Typography variant="h3" fontFamily="inherit" fontWeight={600} component="span"
+                <Box sx={{
+                    marginBottom: {
+                        xs: 0.5,
+                        sm: 1,
+                        md: 1.5,
+                        lg: 2,
+                    }, fontSize: {
+                        xs: "1.5rem",
+                        sm: "2rem",
+                        md: "2.5rem",
+                        lg: "3rem",
+                    }
+                }}>
+                    <Typography variant="h3" fontFamily="inherit" fontWeight={600} fontSize="inherit" component="span"
                                 sx={{marginRight: 1,}}>All in</Typography>
-                    <Typography variant="h3" fontFamily="inherit" fontWeight={600} color="primary" component="span">One
+                    <Typography variant="h3" fontFamily="inherit" fontWeight={600} color="primary" fontSize="inherit"
+                                component="span">One
                         Spot</Typography>
                 </Box>
-                <List sx={{padding: 0, marginBottom: 4, display: "flex", flexDirection: "column", rowGap: 1,}}>
+                <List sx={{
+                    padding: 0, marginBottom: {
+                        xs: 1,
+                        sm: 2,
+                        md: 3,
+                        lg: 4,
+                    }, display: "flex", flexDirection: "column", rowGap: 1,
+                }}>
                     <ListItem sx={{padding: 0}}>
                         <Box sx={{display: "flex", alignItems: "center", columnGap: 1}}>
                             <Box sx={{
-                                width: 26,
-                                height: 26,
+                                width: {
+                                    xs: 20,
+                                    sm: 22,
+                                    md: 24,
+                                    lg: 26,
+                                },
+                                height: {
+                                    xs: 20,
+                                    sm: 22,
+                                    md: 24,
+                                    lg: 26,
+                                },
                                 backgroundColor: "grey.800",
                                 borderRadius: 50,
                                 textAlign: "center",
-                                lineHeight: "26px"
+                                lineHeight: {
+                                    xs: "20px",
+                                    sm: "22px",
+                                    md: "24px",
+                                    lg: "26px",
+                                },
+                                fontSize: {
+                                    xs: "0.7rem",
+                                    sm: "0.8rem",
+                                    md: "0.9rem",
+                                    lg: "1rem",
+                                },
                             }}>
                                 1
                             </Box>
-                            <Typography variant="body1" fontFamily="inherit">Bring all of your content together into one
+                            <Typography variant="body1" fontFamily="inherit" sx={{
+                                fontSize: {
+                                    xs: "0.7rem",
+                                    sm: "0.8rem",
+                                    md: "0.9rem",
+                                    lg: "1rem",
+                                },
+                            }}>Bring all of your content together into one
                                 homepage.</Typography>
                         </Box>
                     </ListItem>
                     <ListItem sx={{padding: 0}}>
                         <Box sx={{display: "flex", alignItems: "center", columnGap: 1}}>
                             <Box sx={{
-                                width: 26,
-                                height: 26,
+                                width: {
+                                    xs: 20,
+                                    sm: 22,
+                                    md: 24,
+                                    lg: 26,
+                                },
+                                height: {
+                                    xs: 20,
+                                    sm: 22,
+                                    md: 24,
+                                    lg: 26,
+                                },
                                 backgroundColor: "grey.800",
                                 borderRadius: 50,
                                 textAlign: "center",
-                                lineHeight: "26px"
+                                lineHeight: {
+                                    xs: "20px",
+                                    sm: "22px",
+                                    md: "24px",
+                                    lg: "26px",
+                                },
+                                fontSize: {
+                                    xs: "0.7rem",
+                                    sm: "0.8rem",
+                                    md: "0.9rem",
+                                    lg: "1rem",
+                                },
                             }}>
                                 2
                             </Box>
-                            <Typography variant="body1" fontFamily="inherit">Your page automatically updates whenever
+                            <Typography variant="body1" fontFamily="inherit" sx={{
+                                fontSize: {
+                                    xs: "0.7rem",
+                                    sm: "0.8rem",
+                                    md: "0.9rem",
+                                    lg: "1rem",
+                                },
+                            }}>Your page automatically updates whenever
                                 you create.</Typography>
                         </Box>
                     </ListItem>
                 </List>
-                <Stack direction="row" columnGap={2}>
+                <Stack alignItems="center" rowGap={1} columnGap={2} sx={{
+                    marginBottom: {
+                        xs: 1,
+                        lg: 2,
+                    },
+                    flexDirection: {
+                        xs: "column",
+                        sm: "row",
+                    },
+                    width: "100%",
+                }}>
                     <Button variant="contained"
-                            sx={{height: 40, width: 150, fontFamily: "inherit", fontSize: 12, fontWeight: 500,}}>
-                        Get Started Now
-                    </Button>
+                            sx={{
+                                fontFamily: "inherit", fontWeight: 400,
+                                width: {
+                                    xs: "100%",
+                                    sm: 150,
+                                    md: 180,
+                                    lg: 200,
+                                },
+                                height: {
+                                    xs: 30,
+                                    md: 40,
+                                    lg: 50,
+                                },
+                                fontSize: {
+                                    xs: 10,
+                                    md: 11,
+                                    lg: 12,
+                                },
+                            }}>Get Started
+                        Now</Button>
                     <GreyButton variant="outlined" sx={{
-                        height: 40, width: 150, fontFamily: "inherit", fontWeight: 500, fontSize: 12,
+                        fontFamily: "inherit", fontWeight: 400,
+                        width: {
+                            xs: "100%",
+                            sm: 150,
+                            md: 180,
+                            lg: 200,
+                        },
+                        height: {
+                            xs: 30,
+                            md: 40,
+                            lg: 50,
+                        },
+                        fontSize: {
+                            xs: 10,
+                            md: 11,
+                            lg: 12,
+                        },
                     }}>View A Demo</GreyButton>
                 </Stack>
             </Box>
-            <Box sx={{position: "relative"}}>
-                <Box sx={{height: 320, width: 444, zIndex: 1, position: "relative",
-                    backgroundImage: `url(${image})`, backgroundRepeat: "no-repeat", backgroundSize: "contain", boxShadow: "0px 30px 50px rgba(0, 0, 0, 0.4)"}}>
+            <Box sx={{
+                position: "relative", display: {
+                    xs: "none",
+                    md: "flex",
+                },
+            }}>
+                <Box sx={{
+                    height: {
+                        md: 210,
+                        lg: 320,
+                    },
+                    width: {
+                        md: 290,
+                        lg: 444,
+                    },
+                    zIndex: 1,
+                    position: "relative",
+                    backgroundImage: `url(${image})`,
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "contain",
+                    boxShadow: "0px 30px 50px rgba(0, 0, 0, 0.4)"
+                }}>
                 </Box>
                 <Box sx={{position: "absolute", bottom: "-40%", left: "50%", transform: "translateX(-50%)"}}>
                     <svg width="200" height="276" viewBox="0 0 309 276" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -71,7 +258,7 @@ const YourHomePage: FC = () => {
                     </svg>
                 </Box>
             </Box>
-        </Box>
+        </Container>
     )
 }
 
