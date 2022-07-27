4c8b6b3e8fb3fed71166079a7c7695c982dbc6c3
Alexey Egorov <krosy1337@gmail.com>
2022-07-28 00:03:57 +0300

    Add adaptive in ExpertsAgree section

--- src/components/pages/HomePage/ExpertsAgree.tsx
+++ src/components/pages/HomePage/ExpertsAgree.tsx
@@ -1,18 +1,41 @@
-import React, {FC} from 'react'
+import React, {FC} from "react"
 import {Box, Container, Typography} from "@mui/material"
 import image from "assets/twitts.jpg"
 
 const ExpertsAgree: FC = () => {
     return (
-        <Container maxWidth="lg" sx={{marginBottom: 5,}}>
-            <Typography variant="h2" align="center" fontFamily="inherit" fontWeight={600} sx={{marginBottom: 5,}}>
+        <Container maxWidth="lg" sx={{
+            marginBottom: {
+                xs: 4,
+                sm: 6,
+                md: 8,
+                lg: 10,
+            },
+        }}>
+            <Typography variant="h2" align="center" fontFamily="inherit" fontWeight={600} sx={{
+                marginBottom: {
+                    xs: 1,
+                    sm: 2,
+                    md: 4,
+                    lg: 5,
+                },
+                fontSize: {
+                    xs: "2rem",
+                    sm: "3rem",
+                    md: "3.5rem",
+                    lg: "3.75rem",
+                }
+            }}>
                 Experts Agree</Typography>
             <Box sx={{
                 height: 280,
                 position: "relative",
                 backgroundImage: `url(${image})`,
                 backgroundRepeat: "no-repeat",
-                backgroundSize: "contain",
+                backgroundSize: {
+                    xs: "cover",
+                    md: "contain",
+                },
                 opacity: 0.45,
             }}>
             </Box>
